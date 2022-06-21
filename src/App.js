edf8ca9e105381e4e433f61e716017fc0bb55c0d
fj_sun <2854805367@qq.com>
2022-06-21 14:56:41 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import Header from './component/Header/index'
 import List from './component/List';
 import Footer from './component/Footer';
-function App() {
-  return (
-    <div className='todo-container'>
-        <div className="todo-wrap">
-            <Header/>
-            <List/>
-            <Footer/>
-        </div>
-    </div>
-  );
+import React, { Component } from 'react'
+export default class App extends Component {
+  state = { todos: [
+    {name:'吃饭',id:1,done:true},
+    {name:'睡觉',id:2,done:true},
+    {name:'敲代码',id:3,done:true},
+  ]}
+  render(){
+     return (
+          <div className='todo-container'>
+              <div className="todo-wrap">
+                  <Header/>
+                  <List  todos={this.state.todos}/>
+                  <Footer/>
+              </div>
+          </div>
+        );
+      } 
 }
-export default App;
