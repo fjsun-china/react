9301ec8075e21bead3015e1f590cf9c072cd759e
fj_sun <2854805367@qq.com>
2022-06-21 14:23:11 +0800

    Add files via upload

--- src/component/List/index.jsx
+++ src/component/List/index.jsx
@@ -3,10 +3,18 @@ import Item from '../Item'
 import './index.css'
 export default class List extends Component {
   render() {
+    console.log(this)
+    const {todos} = this.props;
     return (
       <div className='todo-List'>
-        <Item/>
-      </div>
+          <ul>
+            {
+              todos.map((todo)=>{                 
+                return <Item item={todo} key={todo.id}/>               
+              })
+            }
+          </ul>
+        </div>
     )
   }
 }
