56cee94796d1e5ddedf664a9d06555be7881e28b
fj_sun <2854805367@qq.com>
2022-06-22 17:43:12 +0800

    Add files via upload

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 import './index.css'
 export default class Footer extends Component {
-  
+
+  handleAllTodo = (event)=>{
+    const {handleAllTodos} = this.props;
+    const flag = event.target.checked;
+    handleAllTodos(flag);
+  }
+  clearTodos = ()=>{
+    const {clearTodosDone} = this.props;
+    clearTodosDone();
+  }
   render() {
     const {todos} = this.props
-    const CompleteTodos = todos.filter((item)=>{
+    const todosDone = todos.filter((item)=>{
       return item.done === true;
     })
+    const totalTodos = todos.length;
     return (
       <div className='todo-Footer'>
         <label>
-          <input type="checkbox"></input>
-          <span>已完成{CompleteTodos.length}/全部{todos.length}</span>
+          <input type="checkbox" checked = {totalTodos === todosDone.length &&  totalTodos !==0 ? true : false} onChange = {this.handleAllTodo}></input>
+          <span>已完成{todosDone.length}/全部{todos.length}</span>
         </label>
-        <button>清除已完成任务</button>
+        <button onClick = {this.clearTodos}>清除已完成任务</button>
       </div>
     )
   }
