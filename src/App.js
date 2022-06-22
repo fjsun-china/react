fc4160b247acdb8f08e9a06c9fcaed0fb80066a2
fj_sun <2854805367@qq.com>
2022-06-22 17:42:07 +0800

    Add files via upload
    完善footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Footer from './component/Footer';
 import React, { Component } from 'react';
 export default class App extends Component {
   state = { todos: [
-    {name:'吃饭',id:1,done:true},
-    {name:'睡觉',id:2,done:true},
-    {name:'敲代码',id:3,done:true},
-  ]}
+    {name:'吃饭',id:1,done:false},
+    {name:'睡觉',id:2,done:false},
+    {name:'敲代码',id:3,done:false},
+  ],}
 
   addTodos = (todo)=>{
     const {todos} = this.state;
@@ -26,19 +26,41 @@ export default class App extends Component {
       })
       this.setState({todos:newTodos})
   }
-  deleteTodo = (id)=>{
+  deleteTodos = (id)=>{
     const newTodos = this.state.todos.filter((item)=>{
       return item.id !== id;
     })
     this.setState({todos:newTodos})
+  }
+  handleAllTodos=(checked)=>{
+    const {todos} = this.state;
+    const newTodos = todos.map((item)=>{
+      item.done = checked;
+      return item; 
+    })
+    this.setState({todos:newTodos})
+  }
+  clearTodosDone =()=>{
+    const {todos} = this.state;
+    const newTodos = todos.map((item)=>{
+      item.done = false;
+      return item; 
+    });
+    this.setState({todos:newTodos})
+    const todolist = todos.filter((item)=>{
+      return item.done === false;
+   });
+   if(todolist.length <= 0){
+     this.setState({TodosAllChecked:false})
+   }
   }
   render(){
      return (
           <div className='todo-container'>
               <div className="todo-wrap">
                   <Header addTodos = {this.addTodos}/>
-                  <List  todos={this.state.todos} changeTodoChecked={this.changeTodoChecked} deleteTodo = {this.deleteTodo}/>
-                  <Footer todos={this.state.todos} />
+                  <List  todos={this.state.todos} changeTodoChecked={this.changeTodoChecked} deleteTodos = {this.deleteTodos}/>
+                  <Footer todos={this.state.todos} handleAllTodos={this.handleAllTodos} clearTodosDone={this.clearTodosDone}/>
               </div>
           </div>
         );
